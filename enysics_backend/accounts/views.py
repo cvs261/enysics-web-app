c8d517cf4459b5a7b35faad6eb7240e4a4e16640
cvs261 <vconstantin261@gmail.com>
2025-01-22 15:23:03 +0200

    create auth sys

---
+++ enysics_backend/accounts/views.py
@@ -0,0 +1,44 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from .serializers import RegisterSerializer, LoginSerializer
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                "user": {
+                    "username": user.username,
+                    "email": user.email,
+                },
+                "token": token.key
+            })
+        return Response(serializer.errors, status=400)
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({
+                    "user": {
+                        "username": user.username,
+                        "email": user.email,
+                    },
+                    "token": token.key
+                })
+            return Response({"error": "Invalid credentials"}, status=400)
+        return Response(serializer.errors, status=400)
--- enysics_backend/enysics_backend/settings.py
+++ enysics_backend/enysics_backend/settings.py
@@ -27,17 +27,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Application definition
 
 INSTALLED_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
